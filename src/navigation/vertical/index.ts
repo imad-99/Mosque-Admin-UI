0ee7c6785d7756cacec2fdb62a35a27deb622a86
imad-99 <imadelfetouh@hotmail.com>
2023-10-10 10:30:30 +0200

    add home page

--- src/navigation/vertical/index.ts
+++ src/navigation/vertical/index.ts
@@ -2,6 +2,11 @@ import type { VerticalNavItems } from '@/@layouts/types'
 
 export default [
   {
+    title: 'Home',
+    to: { name: 'index' },
+    icon: { icon: 'tabler-smart-home' },
+  },
+  {
     title: 'Djanazah',
     to: { name: 'djanazah' },
     icon: { icon: 'tabler-mood-sad' }
---
+++ src/pages/index.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { Component, toNative, Vue } from 'vue-facing-decorator';
+import DjanazahPage from './djanazah/index.vue';
+
+@Component({
+  components: {
+    DjanazahPage
+  }
+})
+class HomePage extends Vue {
+}
+
+export default toNative(HomePage)
+</script>
+
+<template>
+    <DjanazahPage></DjanazahPage>
+</template>
